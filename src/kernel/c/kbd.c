884250d06e7811d5d6d28377b7d5f4e637a28cff
bg117 <kiangabrielarambulo@gmail.com>
2022-06-30 21:23:09 +0800

    add shift functionality to keyboard handler

--- src/kernel/c/kbd.c
+++ src/kernel/c/kbd.c
@@ -46,6 +46,39 @@ static const char US_SCANCODES[128] = {
     0, /* All other keys are undefined */
 };
 
+static const char US_SCANCODES_UPR[128] = {
+    0,    27,  '!', '@', '#',  '$', '%', '^', '&',  '*', /* 9 */
+    '(',  ')', '_', '+', '\b',                           /* Backspace */
+    '\t',                                                /* Tab */
+    'Q',  'W', 'E', 'R',                                 /* 19 */
+    'T',  'Y', 'U', 'I', 'O',  'P', '{', '}', '\n',      /* Enter key */
+    0,                                                   /* 29   - Control */
+    'A',  'S', 'D', 'F', 'G',  'H', 'J', 'K', 'L',  ':', /* 39 */
+    '"',  '~', 0,                                        /* Left shift */
+    '|',  'Z', 'X', 'C', 'V',  'B', 'N',                 /* 49 */
+    'M',  '<', '>', '?', 0,                              /* Right shift */
+    '*',  0,                                             /* Alt */
+    ' ',                                                 /* Space bar */
+    0,                                                   /* Caps lock */
+    0,                                                   /* 59 - F1 key ... > */
+    0,    0,   0,   0,   0,    0,   0,   0,   0,         /* < ... F10 */
+    0,                                                   /* 69 - Num lock*/
+    0,                                                   /* Scroll Lock */
+    0,                                                   /* Home key */
+    0,                                                   /* Up Arrow */
+    0,                                                   /* Page Up */
+    '-',  0,                                             /* Left Arrow */
+    0,    0,                                             /* Right Arrow */
+    '+',  0,                                             /* 79 - End key*/
+    0,                                                   /* Down Arrow */
+    0,                                                   /* Page Down */
+    0,                                                   /* Insert Key */
+    0,                                                   /* Delete Key */
+    0,    0,   0,   0,                                   /* F11 Key */
+    0,                                                   /* F12 Key */
+    0, /* All other keys are undefined */
+};
+
 static char         _last_char;
 static char         _last_scancode;
 static volatile int _got_key;
@@ -79,6 +112,13 @@ char kbd_get_char()
     return _last_char;
 }
 
+#define NO_TOP_BIT(x)  ((x)&0x7F)
+#define SHIFT_CHECK(x) (NO_TOP_BIT(x) == 0x2A || NO_TOP_BIT(x) == 0x36)
+#define ALT_CHECK(x)   (NO_TOP_BIT(x) == 0x38)
+#define CTRL_CHECK(x)  (NO_TOP_BIT(x) == 0x1D)
+#define SHIFT_ALT_CTRL_CHECK(x)                                                \
+    (SHIFT_CHECK(x) || ALT_CHECK(x) || CTRL_CHECK(x))
+
 void _irq1_handler(struct exception_info *info)
 {
     uint8_t read;
@@ -95,14 +135,39 @@ void _irq1_handler(struct exception_info *info)
 
     if ((read & 0x80))
     {
-        // TODO: implement key-release functionality
+        if (SHIFT_ALT_CTRL_CHECK(read))
+        {
+            if (SHIFT_CHECK(read))
+                _key_flags ^= KEY_SHIFT;
+
+            if (ALT_CHECK(read))
+                _key_flags ^= KEY_ALT;
+
+            if (CTRL_CHECK(read))
+                _key_flags ^= KEY_CTRL;
+        }
     }
     else
     {
-        _last_char     = US_SCANCODES[read];
-        _last_scancode = read;
+        if (SHIFT_ALT_CTRL_CHECK(read))
+        {
+            if (SHIFT_CHECK(read))
+                _key_flags ^= KEY_SHIFT;
 
-        _got_key = 1;
+            if (ALT_CHECK(read))
+                _key_flags ^= KEY_ALT;
+
+            if (CTRL_CHECK(read))
+                _key_flags ^= KEY_CTRL;
+        }
+        else
+        {
+            _last_char = (_key_flags & KEY_SHIFT) == 1 ? US_SCANCODES_UPR[read]
+                                                       : US_SCANCODES[read];
+            _last_scancode = read;
+
+            _got_key = 1;
+        }
     }
 
     pic_send_eoi(0x01);
